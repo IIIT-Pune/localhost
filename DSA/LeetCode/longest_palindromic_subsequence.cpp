a2ce68ecc391b9bbb9b204b7c80553395b9cd5a9
Khemendra Bhardwaj <khemendra123b@gmail.com>
2022-10-19 00:41:01 +0530

    updated comments and time and space complexity

--- DSA/LeetCode/longest_palindromic_subsequence.cpp
+++ DSA/LeetCode/longest_palindromic_subsequence.cpp
@@ -33,3 +33,22 @@ signed main() {
 	cout << LPS(X, n) << endl;
 	return 0;
 }
+
+// Time Complexity: O(n^2)
+// Auxiliary Space: O(n^2),  Creating a table
+
+// Steps Below : for reference 
+
+
+// // Every single character is a palindrome of length 1
+// L(i, i) = 1 for all indexes i in given sequence
+
+// // IF first and last characters are not same
+// If (X[i] != X[j])  L(i, j) =  max{L(i + 1, j),L(i, j - 1)} 
+
+// // If there are only 2 characters and both are same
+// Else if (j == i + 1) L(i, j) = 2  
+
+// // If there are more than two characters, and first and last 
+// // characters are same
+// Else L(i, j) =  L(i + 1, j - 1) + 2 
